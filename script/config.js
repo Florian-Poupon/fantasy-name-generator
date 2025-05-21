087ab821299fdb9965dc578e96ff7c27afe76ddf
Florian Poupon <pro.poupon.fl@gmail.com>
2025-05-21 15:20:45 +0200

    V1 JS fonctionnel

---
+++ script/config.js
@@ -0,0 +1,44 @@
+//Races
+
+const listeRaces = ["humain", "nain", "elfe", "gnome", "orc"];
+const listeSexe = ["homme", "femme"];
+
+//noms Humains/Humaines
+
+const prefixesHumainMasculin = ["Alar", "Bren", "Cal", "Dar", "Gar", "Hal", "Jan", "Mal", "Ron", "Tiber"];
+const suffixesHumainMasculin = ["dor", "an", "ius", "ar", "en", "us", "ton", "ald", "eric", "vin"];
+
+const prefixesHumainFeminin = ["Elen", "Mara", "Lysa", "Calia", "Jena", "Sora", "Alia", "Mira", "Talia", "Rina"];
+const suffixesHumainFeminin = ["elle", "ana", "ia", "ine", "essa", "aria", "yne", "etta", "ora"];
+
+// noms Nains
+
+const prefixesNainMasculin = ["Bal", "Dain", "Thor", "Grum", "Dur", "Kaz", "Thrain", "Brok", "Grom", "Fund"];
+const suffixesNainMasculin = ["in", "son", "dar", "grim", "or", "rek", "mund", "ogrin", "nor"];
+
+const prefixesNainFeminin = ["Brunna", "Gilda", "Katra", "Thora", "Gunna", "Frida", "Helga", "Dorga", "Morna", "Vilda"];
+const suffixesNainFeminin = ["la", "ina", "gna", "lda", "hild", "mara", "runa", "vara", "nys"];
+
+// noms Elfes
+
+const prefixesElfeMasculin = ["Ael", "Eri", "Luth", "Syl", "Thael", "Faer", "Ille", "Cael", "Ven"];
+const suffixesElfeMasculin = ["rion", "thas", "nor", "las", "enor", "adel", "mir", "aran"];
+
+const prefixesElfeFeminin = ["Aelya", "Lúria", "Seris", "Elira", "Yllia", "Naeva", "Thelya", "Valya", "Shae"];
+const suffixesElfeFeminin = ["wyn", "riel", "thiel", "loth", "enna", "aria", "ora", "ael"];
+
+// noms Gnomes
+
+const prefixesGnomeMasculin = ["Fiz", "Bim", "Nix", "Gim", "Wob", "Zib", "Pim", "Rix"];
+const suffixesGnomeMasculin = ["wick", "bork", "tix", "oddle", "dibble", "bop", "frick", "nix"];
+
+const prefixesGnomeFeminin = ["Pippa", "Zilly", "Tinka", "Lolla", "Minna", "Zuna", "Gilly", "Dotti", "Brilla"];
+const suffixesGnomeFeminin = ["ybelle", "tinka", "lilla", "opple", "dora", "mella", "bitta", "wina", "zetta"];
+
+// noms Ors
+
+const prefixesOrcMasculin = ["Grok", "Urg", "Krag", "Mok", "Thok", "Grol", "Zuk", "Brak", "Krog"];
+const suffixesOrcMasculin = ["mak", "zug", "thrak", "nok", "dur", "argh", "um", "nog", "zul"];
+
+const prefixesOrcFeminin = ["Grolka", "Sharga", "Ugra", "Zorga", "Krasha", "Vorka", "Draga", "Brulka", "Zunra"];
+const suffixesOrcFeminin = ["akha", "gra", "orra", "uka", "zha", "nasha", "vra", "zra", "sha"];
---
+++ script/script.js
@@ -0,0 +1,101 @@
+//Random Number
+
+function randomNbr(liste) {
+  return liste[Math.floor(Math.random() * liste.length)];
+}
+
+//clic race
+const raceClic = document.querySelectorAll(".raceBtn button");
+const background = document.getElementById("background");
+
+raceClic.forEach((button) => {
+  button.addEventListener("click", () => {
+    let race = button.getAttribute("data-race");
+
+    //changer background
+    if (!background) return;
+    background?.classList.add("fade-out");
+    void background.offsetWidth;
+    setTimeout(() => {
+      background.style.backgroundImage = `url('images/background/background-${race}.png')`;
+      background.classList.remove("fade-out");
+    }, 100);
+
+    raceClic.forEach((button) => button.classList.remove("active"));
+    button.classList.add("active");
+  });
+});
+
+//clic genre
+const genreClic = document.querySelectorAll(".genreBtn button");
+
+genreClic.forEach((button) => {
+  button.addEventListener("click", () => {
+    let genre = button.getAttribute("data-genre");
+    genreClic.forEach((button) => button.classList.remove("active"));
+    button.classList.add("active");
+  });
+});
+
+// création du nom
+function generateName() {
+  const raceChoice = document.querySelector(".raceBtn button.active");
+  const genreChoice = document.querySelector(".genreBtn button.active");
+
+  const raceData = raceChoice?.getAttribute("data-race");
+  const genreData = genreChoice?.getAttribute("data-genre");
+
+  let nom = "";
+
+  if (raceData === "humain") {
+    if (genreData === "masculin") {
+      nom = randomNbr(prefixesHumainMasculin) + randomNbr(suffixesHumainMasculin);
+    } else {
+      nom = randomNbr(prefixesHumainFeminin) + randomNbr(suffixesHumainFeminin);
+    }
+  }
+  if (raceData === "elfe") {
+    if (genreData === "masculin") {
+      nom = randomNbr(prefixesElfeMasculin) + randomNbr(suffixesElfeMasculin);
+    } else {
+      nom = randomNbr(prefixesElfeFeminin) + randomNbr(suffixesElfeFeminin);
+    }
+  }
+  if (raceData === "nain") {
+    if (genreData === "masculin") {
+      nom = randomNbr(prefixesNainMasculin) + randomNbr(suffixesNainMasculin);
+    } else {
+      nom = randomNbr(prefixesNainFeminin) + randomNbr(suffixesNainFeminin);
+    }
+  }
+  if (raceData === "gnome") {
+    if (genreData === "masculin") {
+      nom = randomNbr(prefixesGnomeMasculin) + randomNbr(suffixesGnomeMasculin);
+    } else {
+      nom = randomNbr(prefixesGnomeFeminin) + randomNbr(suffixesGnomeFeminin);
+    }
+  }
+  if (raceData === "orc") {
+    if (genreData === "masculin") {
+      nom = randomNbr(prefixesOrcMasculin) + randomNbr(suffixesOrcMasculin);
+    } else {
+      nom = randomNbr(prefixesOrcFeminin) + randomNbr(suffixesOrcFeminin);
+    }
+  }
+  return nom;
+}
+
+//Afficher le nom
+
+function afficherNom() {
+  let generateBtn = document.querySelector(".generateBtn button");
+  generateBtn?.addEventListener("click", () => {
+    let monNom = generateName();
+
+    const yourName = document.querySelector(".yourName p");
+    if (!yourName) return;
+    yourName.textContent = `Votre nom est "${monNom}" !`;
+  });
+}
+
+afficherNom();
